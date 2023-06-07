0e42e1d9df156ef143f37f694042f62fc544a33d
nickciava98 <n.ciavarella@outlook.it>
2023-06-07 21:35:15 +0200

    Delete

--- nciavarella_custom/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import account_move
-from . import product_product
-from . import activity_costs
--- nciavarella_custom/models/product_product.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-
-    is_hourly_cost = fields.Boolean(
-        store = True
-    )
