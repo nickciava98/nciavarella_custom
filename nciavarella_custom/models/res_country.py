6faabfbead1cec7664dedfb6e62a873d46f5514f
nickciava98 <n.ciavarella@outlook.it>
2024-07-16 17:31:01 +0200

    Update visualizzazione indirizzi

--- nciavarella_custom/models/res_country.py
+++ nciavarella_custom/models/res_country.py
@@ -4,6 +4,18 @@ from odoo import models, api, exceptions, _
 class ResCountry(models.Model):
     _inherit = "res.country"
 
+    @api.model
+    def update_it_address_format(self):
+        country_id = self.env.ref("base.it", False)
+
+        if not country_id:
+            return
+
+        address_format = (
+            "%(street)s, %(numero_civico)s\n%(street2)s\n%(zip)s, %(city)s (%(state_code)s), %(country_name)s"
+        )
+        country_id.write({"address_format": address_format})
+
     @api.constrains("address_format")
     def _check_address_format(self):
         for country in self:
