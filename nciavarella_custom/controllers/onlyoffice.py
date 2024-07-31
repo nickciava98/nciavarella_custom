3a24af1ed67556161979fd8ccfaeb03aa7a46797
nickciava98 <n.ciavarella@outlook.it>
2024-07-31 15:57:20 +0200

    Update controller onlyoffice

--- nciavarella_custom/controllers/onlyoffice.py
+++ nciavarella_custom/controllers/onlyoffice.py
@@ -1,20 +1,12 @@
-import ast
 import markupsafe
-import json
 
-from odoo.http import request
 from odoo.addons.onlyoffice_odoo.controllers.controllers import Onlyoffice_Connector
 
 
 class OnlyOfficeConnectorInherit(Onlyoffice_Connector):
     def prepare_editor_values(self, attachment, access_token, can_write):
         values = super().prepare_editor_values(attachment, access_token, can_write)
-        root_config = ast.literal_eval(values.get("editorConfig"))
-        editor_config = root_config.get("editorConfig")
-        user = editor_config.get("user")
-        user["name"] = request.env.user.name.replace("'", "")
-        editor_config["user"] = user
-        root_config["editorConfig"] = editor_config
-        values["editorConfig"] = markupsafe.Markup(json.dumps(root_config))
+        root_editor_config = str(values.get("editorConfig")).replace("'", "")
+        values["editorConfig"] = markupsafe.Markup(root_editor_config)
 
         return values
