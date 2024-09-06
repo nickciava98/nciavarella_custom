ce666fd4932e71c61a9703edbcfc21ceda248476
nickciava98 <n.ciavarella@outlook.it>
2024-09-06 10:34:47 +0200

    Tipo attività Fogli Ore

--- nciavarella_custom/__manifest__.py
+++ nciavarella_custom/__manifest__.py
@@ -5,7 +5,7 @@
     "license": "OPL-1",
     "author": "Niccolò Ciavarella",
     "category": "",
-    "version": "16.0.0.34",
+    "version": "16.0.0.35",
     "website": "http://www.nciavarella.me",
     "depends": [
         "l10n_it_edi",
